32ac04f809542629f1982306ab995cc6f4d9164d
Francisco Alanzaves <Francisco Alanzaves@DESKTOP-SIBDLOH>
2022-07-04 12:58:47 +0200

    Primer commit

---
+++ src/main/java/com/codigoagil/EjemploVehiculosApplication.java
@@ -0,0 +1,13 @@
+package com.codigoagil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploVehiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploVehiculosApplication.class, args);
+	}
+
+}
